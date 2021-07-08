6234dd30fa7aef5bf27d83d5787c8d611ffedeb0
Linde Huang <linde.lu.huang@gmail.com>
2021-07-08 17:35:01 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '870689293827408',
+        clientSecret: '54a666e80a6cd99d0846c5325886b362'
+    }
 }
